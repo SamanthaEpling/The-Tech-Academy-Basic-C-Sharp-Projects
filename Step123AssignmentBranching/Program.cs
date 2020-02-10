3908c0cd58e8801b464f6d5db1f86fa2804e98cc
SamanthaEpling <alleycatace@yahoo.com>
2020-02-09 20:57:31 -0800

    Push/Commit for Step 123
    Pushing/Commit Files for Step 123 Assignment; C# & .NET Framework Course; February 9, 2020.

---
+++ Step123AssignmentBranching/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Step123AssignmentBranching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express.  Please follow the instructions below.");
+            Console.WriteLine("Please enter the package weight:");
+            decimal p_weight = Convert.ToDecimal(Console.ReadLine());
+            if (p_weight > 50)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express.  Have a good day.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Please enter the package width:");
+                decimal p_width = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please enter the package height:");
+                decimal p_height = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Please enter the package length:");
+                decimal p_length = Convert.ToDecimal(Console.ReadLine());
+                
+                if ((p_width > 50) && (p_height > 50) && (p_length > 50))
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express.");
+                }
+                else
+                {
+                    decimal p_cost = (p_width * p_height * p_length * p_weight) / 100;
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + p_cost + ".");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Step123AssignmentBranching/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Step123AssignmentBranching")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Step123AssignmentBranching")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b4438504-2852-4b92-9b7f-c95d8ce4ca4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
