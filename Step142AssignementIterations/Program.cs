2830f8f0e8706866ef324287fa066a86ab750771
SamanthaEpling <alleycatace@yahoo.com>
2020-02-16 17:06:35 -0800

    Uploading files for Step 142
    Uploading files for Step/Assignment 142; C# & .NET Framework Course; The Tech Academy; February 16, 2020;

--- Step142AssignementIterations/Program.cs
+++ Step142AssignementIterations/Program.cs
@@ -8,16 +8,16 @@ class Program
     //=====Section1: Array of strings with a loop with user input======
 
     //{
-    //Console.WriteLine("Type a sentence for every single one of the pets: ");
-    //string sentence = Console.ReadLine();
-    //string[] names = { "Lionel", "Romani", "Rosie", "Fiona" };
+    //    Console.WriteLine("Type a sentence for every single one of the pets: ");
+    //    string sentence = Console.ReadLine();
+    //    string[] names = { "Lionel", "Romani", "Rosie", "Fiona" };
 
-    //for (int i = 0; i < names.Length; i++)
-    //{
-    //    Console.WriteLine(names[i] + sentence);
+    //    for (int i = 0; i < names.Length; i++)
+    //    {
+    //        Console.WriteLine(names[i] + sentence);
+    //    }
+    //    Console.ReadLine();
     //}
-    //Console.ReadLine();
-
     //=====Section2: to create an infinite loop:=====
 
     //{
@@ -35,29 +35,29 @@ class Program
 
     //{
     //    string[] names = { "Lionel", "Romani", "Rosie", "Fiona" };
-    //int i = 0;
+    //    int i = 0;
     //    do
     //    {
     //        Console.WriteLine(names[i] + " is a great pet!");
     //        i++;
     //    }
-    //    while (i< 4);
+    //    while (i < 4);
     //    Console.ReadLine();
     //}
 
     //=====Section 4: loop with < comparison:======
 
     //{
-    //List<int> petAges = new List<int>() { 5, 22, 10, 7 };
+    //    List<int> petAges = new List<int>() { 5, 22, 10, 7 };
 
-    //foreach (int age in petAges)
-    //{
-    //    if (age < 7)
+    //    foreach (int age in petAges)
     //    {
-    //        Console.WriteLine(age + ": This pet's age would eat an adult diet.");
+    //        if (age < 7)
+    //        {
+    //            Console.WriteLine(age + ": This pet's age would eat an adult diet.");
+    //        }
     //    }
-    //}
-    //Console.ReadLine();
+    //    Console.ReadLine();
     //}
 
     //=====Section 5: loop with <= comparison:======
@@ -66,14 +66,14 @@ class Program
     //{
     //    List<int> petAges = new List<int>() { 5, 22, 10, 7 };
 
-    //        foreach (int age in petAges)
+    //    foreach (int age in petAges)
+    //    {
+    //        if (age >= 7)
     //        {
-    //            if (age >= 7)
-    //            {
-    //                Console.WriteLine(age + ": This pet's age would eat an mature (senior) diet.");
-    //            }
+    //            Console.WriteLine(age + ": This pet's age would eat a mature (senior) diet.");
     //        }
-    //        Console.ReadLine();
+    //    }
+    //    Console.ReadLine();
     //}
 
     //=====Section 6: List of strings with user search for index of array:======
@@ -96,45 +96,27 @@ class Program
 
     //=====Section 7: Adding code if the user was ineffective ;) :======
 
-    {
-        List<string> Pets = new List<string>() { "Lionel", "Romani", "Rosie", "Fiona" };
+    //{
+    //    List<string> Pets = new List<string>() { "Lionel", "Romani", "Rosie", "Fiona" };
 
-        Console.WriteLine("Please type a pet name to see who is on the list: ");
-        string chosenName = Console.ReadLine();
+    //    Console.WriteLine("Please type a pet name to see who is on the list: ");
+    //    string chosenName = Console.ReadLine();
+    //    bool isPresent = false;
 
-        foreach (string pet in Pets)
-        {
-            if (pet == chosenName)
-            {
-                Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + Pets.IndexOf(pet));
-                break;
-            }
-            else
-            {
-                Console.WriteLine("Oh darn, that name is not on our list.");
-            }
-        }
-        
-        
-        //for (int i = 0; i < Pets.Count; i++)
-        //{
-        //    if (Pets[i] == chosenName)
-        //    {
-        //        Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + i);
-        //        break;
-        //    }
-
-
-        //    else if (chosenName != Pets[i])
-        //    {
-        //        Console.WriteLine("Oh darn, that name is not on our list.");
-        //        Console.WriteLine("Please type a pet name to see who is on the list: ");
-        //        chosenName = Console.ReadLine();
-
-        //    }
-        //}
-        Console.ReadLine();
-    }
+    //    foreach (string pet in Pets)
+    //    {
+    //        if (pet == chosenName)
+    //        {
+    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + Pets.IndexOf(pet));
+    //            isPresent = true;
+    //        }
+    //    }
+    //    if (!isPresent)
+    //    {
+    //        Console.WriteLine("Oh darn, that name is not on our list.");
+    //    }
+    //    Console.ReadLine();
+    //}
 
     //=====Section 8: Adding code to stop code once user was effective:======
 
@@ -143,21 +125,20 @@ class Program
 
     //    Console.WriteLine("Please type a pet name to see who is on the list: ");
     //    string chosenName = Console.ReadLine();
+    //    bool isPresent = false;
 
-    //    for (int i = 0; i<Pets.Count; i++)
+    //    foreach (string pet in Pets)
     //    {
-    //        if (Pets[i] == chosenName)
+    //        if (pet == chosenName)
     //        {
-    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + i);
+    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + Pets.IndexOf(pet));
+    //            isPresent = true;
     //            break;
     //        }
-    //        else
-    //        {
-    //            Console.WriteLine("Oh darn, that name is not on our list.");
-    //            Console.WriteLine("Please type a pet name to see who is on the list: ");
-    //            chosenName = Console.ReadLine();
-    //            continue;
-    //        }
+    //    }
+    //    if (!isPresent)
+    //    {
+    //        Console.WriteLine("Oh darn, that name is not on our list. Please try again.");
     //    }
     //    Console.ReadLine();
     //}
@@ -168,24 +149,58 @@ class Program
     //    List<string> Pets = new List<string>() { "Lionel", "Romani", "Rosie", "Fiona", "Love Bug", "Mister", "Romani" };
     //    Console.WriteLine("Please type a pet name to see who is on the list: ");
     //    string chosenName = Console.ReadLine();
-
     //    for (int i = 0; i < Pets.Count; i++)
     //    {
     //        if (Pets[i] == chosenName)
     //        {
-    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index: " + i);
-    //            break;
+    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index(es): " + i);
+    //            isPresent = true;
     //        }
-    //            else
-    //            {
-    //                Console.WriteLine("Oh darn, that name is not on our list.");
-    //                Console.WriteLine("Please type a pet name to see who is on the list: ");
-    //                chosenName = Console.ReadLine();
-    //                continue;
-    //            }
     //    }
     //    Console.ReadLine();
+    //}
 
+    //=====Section 10: Adding code to stop code once user was effective:======
+
+    //{
+    //    List<string> Pets = new List<string>() { "Lionel", "Romani", "Rosie", "Fiona", "Love Bug", "Mister", "Romani" };
+    //    Console.WriteLine("Please type a pet name to see who is on the list: ");
+    //    string chosenName = Console.ReadLine();
+    //    bool isPresent = false;
+    //    for (int i = 0; i<Pets.Count; i++)
+    //    {
+    //        if (Pets[i] == chosenName)
+    //        {
+    //            Console.WriteLine("Congratulations! " + chosenName + " was on the pet list at index(es): " + i);
+    //            isPresent = true;
+    //        }
+    //    }
+    //    if (!isPresent)
+    //    {
+    //        Console.WriteLine("Oh darn, that name is not on our list. Please try again.");
+    //    }
+    //    Console.ReadLine();
+    //}
+
+    //=====Section 11: Selecting and indicating identical strings through iterations:======
+
+    //{
+    //    List<string> Pets = new List<string>() { "Lionel", "Romani", "Rosie", "Fiona", "Love Bug", "Mister", "Romani", "Fiona" };
+    //    List<string> ReplicatePets = new List<string>();
+        
+    //    foreach (string pet in Pets)
+    //    {
+    //        if ( ReplicatePets.Contains(pet))
+    //        {
+    //            Console.WriteLine(pet + " has now already appeared on the list multiple times!");  
+    //        }
+    //        else
+    //        {
+    //            ReplicatePets.Add(pet);
+    //            Console.WriteLine(pet + " has been listed at least this once.");
+    //        }
+    //    }
+    //    Console.ReadLine();
     //}
 
 }
