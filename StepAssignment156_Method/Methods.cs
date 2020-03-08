298854c38d33e8ce9acf5e6a44febc0f7b6555bb
SamanthaEpling <alleycatace@yahoo.com>
2020-03-07 18:26:09 -0800

    Files for Step/Assignment156 Methods
    Uploading files for Step/Assignment 156 Methods; C# & .NET Framework Course; The Tech Academy; March 7, 2020.

---
+++ StepAssignment156_Method/Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment156_Method
+{
+    public class Operation1
+    {
+        //constructor:
+        public static int TimesTen(int givenNumber)
+        {
+
+            return givenNumber * 10;
+        }
+    }
+
+
+    public class Operation2
+    {
+        public static int SquareIt(int givenNumber)
+        {
+            return givenNumber ^ 2;
+        }
+    }
+
+
+    public class Operation3
+    {
+        public static int TimesTwenty(int givenNumber)
+        {
+            return givenNumber * 20;
+        }
+    }
+}
---
+++ StepAssignment156_Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StepAssignment156_Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick your favorite whole number.");
+            int givenNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You chose: " + givenNumber + ".");
+            Console.WriteLine("Press enter to multiply " + givenNumber + " by 10.");
+            Console.ReadLine();
+            Console.WriteLine(Operation1.TimesTen(givenNumber));
+            Console.ReadLine();
+            Console.WriteLine("Press enter to square " + givenNumber + ".");
+            Console.ReadLine();
+            Console.WriteLine(Operation2.SquareIt(givenNumber));
+            Console.ReadLine();
+            Console.WriteLine("Press enter to multiply " + givenNumber + " by 20.");
+            Console.ReadLine();
+            Console.WriteLine(Operation3.TimesTwenty(givenNumber));
+            Console.WriteLine("Thanks for playing my little game!");
+            Console.ReadLine();
+
+            
+        }
+    }
+}
