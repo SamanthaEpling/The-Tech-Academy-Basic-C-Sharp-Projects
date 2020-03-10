567b30c44467cc1071e8fee92d74c9a891118655
SamanthaEpling <alleycatace@yahoo.com>
2020-03-09 20:40:28 -0700

    Uploading code for Step/Assignment 160 Methods continued
    Uploading code for Step/Assignment 160 Methods continued; C# & .NET Framework Course; The Tech Academy; March 9, 2020.

---
+++ StepAssignment160Methodscontinued/Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment160Methodscontinued
+{
+    public class Method
+    {
+        public static int AddTwo(int numberOne, int optionTwo = 0)
+        {
+            return (numberOne + optionTwo);
+        }
+    }
+}
---
+++ StepAssignment160Methodscontinued/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment160Methodscontinued
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick your favorite whole number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You chose: " + numberOne + " for your first number.");
+            Console.WriteLine("You may choose a second number or leave it blank.");
+            string numberTwo = Console.ReadLine();
+            int optionTwo = 0;
+            if (numberTwo == "")
+            {
+                numberTwo = "0";
+                optionTwo = Convert.ToInt32(numberTwo);
+            }
+            else
+            {
+                optionTwo = Convert.ToInt32(numberTwo);
+            }
+            Console.WriteLine("Now, their sum!:");
+            Console.WriteLine(Method.AddTwo(numberOne, optionTwo));
+            Console.ReadLine();
+        }
+    }
+}
