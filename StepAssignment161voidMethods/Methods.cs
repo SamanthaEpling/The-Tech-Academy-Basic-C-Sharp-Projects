7fb4fb375dc532345e1eb660c1c48c5e8ac13bae
SamanthaEpling <alleycatace@yahoo.com>
2020-03-11 09:39:11 -0700

    Uploading files for Step/Assignment 161: Void Methods
    Uploading files for Step/Assignment #161 (Void Methods); C# & .NET Framework; The Tech Academy; March 11, 2020,

---
+++ StepAssignment161voidMethods/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment161voidMethods
+{
+    public class Methods
+    {
+        public static void Calc1(int numberOne, int numberTwo, out bool result)
+        {
+            result = (numberOne * numberOne) > (numberTwo * numberTwo);
+        }
+    }
+}
---
+++ StepAssignment161voidMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment161voidMethods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide an integer: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please provide a second integer: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Calculating..,\r\n");
+            bool result;
+            Methods.Calc1(numberOne, numberTwo, out result);
+            Convert.ToString(result);
+            Console.WriteLine("The square of the first number is larger than the square of " + numberTwo + ": \r\n" + result);
+            Console.ReadLine();
+
+        }
+    }
+}
