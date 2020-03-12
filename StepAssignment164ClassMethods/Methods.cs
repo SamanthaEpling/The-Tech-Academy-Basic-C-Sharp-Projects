7b3bbd367820399edb90a24216775f29b6268dc3
SamanthaEpling <alleycatace@yahoo.com>
2020-03-12 08:33:39 -0700

    StepAssignment 164 Class Methods
    Uploading files associated with Step/Assignment 164: Methods: Class Methods; C# & .NET Framework Course; The Tech Academy; March 12, 2020.

---
+++ StepAssignment164ClassMethods/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment164ClassMethods
+{
+    public class Methods
+    {
+        public static void DivideByTwo(int numberOne, out int result)
+        {
+            result = numberOne / 2;
+        }
+        public static int DivideByQuarter(int numberTwo, out int result2)
+        {
+            result2 = numberTwo * (1 / 4);
+            return result2;
+        }
+
+        public static decimal DivideByQuarter(decimal numberThree, out decimal result3)
+        {
+            result3 = numberThree / 4;
+            return result3;
+        }
+    }
+}
---
+++ StepAssignment164ClassMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment164ClassMethods
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide an integer: ");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Calculating your integer's half.....\r\n");
+            int result;
+            Methods.DivideByTwo(numberOne, out result);
+            Console.WriteLine("Half of your integer is: " + result + ".\r\n");
+            Console.WriteLine("But wait! \r\nThere's more! \r\nPlease provide another integer: ");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Calculating your integer's value if multiplied by a quarter......\r\n");
+            int result2;
+            Methods.DivideByQuarter(numberTwo, out result2);
+            Console.WriteLine("The result of multiplying your integer by a quarter is: \r\n" + result2 + ".");
+            Console.WriteLine("But wait! \r\nThere is even more! \r\nPlease provide another integer: ");
+            decimal numberThree = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Calculating your a quarter of your integer....");
+            decimal result3;
+            Methods.DivideByQuarter(numberThree, out result3);
+            Console.WriteLine("The result of dividing your integer by four is: \r\n" + result3 + ".\r\n" +
+                "Thanks for playing my little game.");
+            Console.ReadLine();
+        }
+    }
+}
