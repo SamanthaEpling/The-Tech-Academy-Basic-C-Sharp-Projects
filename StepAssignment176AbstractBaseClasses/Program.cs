d40e08351788ef3d7f2430da9e705f8215fe6c20
SamanthaEpling <alleycatace@yahoo.com>
2020-03-13 11:56:30 -0700

    Step Assignment # 176: Abstract Base Classes
    Files for Step/Assignment 176 (Abstract Base Classes); C# & .NET Framework Course; The Tech Academy; March, 13, 2020.

---
+++ StepAssignment176AbstractBaseClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment176AbstractBaseClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.lastName = "Student";
+            employee.firstName = "Sample";
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
