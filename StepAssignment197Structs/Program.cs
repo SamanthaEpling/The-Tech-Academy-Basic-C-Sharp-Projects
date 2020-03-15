5d62f78ed42c2d4fb2c4a4db3b2bc4983155405c
SamanthaEpling <alleycatace@yahoo.com>
2020-03-14 20:55:38 -0700

    Pushing for Step/Assignment # 197: Structs
    Submitting files for Step/Assignment # 197 (Structs); C# & .NET Framework Course; The Tech Academy; March 14, 2020.

---
+++ StepAssignment197Structs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment197Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number1 = new Number();
+            number1.Amount = 3.14159265M;
+
+            Console.WriteLine("Happy Pi day! Pi is: " + number1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
