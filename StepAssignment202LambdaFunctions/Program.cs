3e7e65a9d4dddb46887580d1b53eaca86cccb519
SamanthaEpling <alleycatace@yahoo.com>
2020-03-18 10:49:13 -0700

    Step Assignment 202 Lambda Functions
    Uploading final submission for Step/Assignment # 202 (Lambda Functions); C# & .NET Framework Course; The Tech Academy; March 18, 2020.

--- StepAssignment202LambdaFunctions/Program.cs
+++ StepAssignment202LambdaFunctions/Program.cs
@@ -14,76 +14,76 @@ namespace StepAssignment202LambdaFunctions
             // ======================= Sections 1 & 2: using the foreach loop ============================
 
 
-            List<string> Employee = new List<string>();
-            List<string> JoeList = new List<string>();
+            List<Employee> empList = new List<Employee>();
+            List<Employee> selectedEmployee = new List<Employee>();
 
             Employee employee1 = new Employee();
             employee1.fName = "Joe";
             employee1.lName = "Smith";
             employee1.IDnumber = 1;
+            empList.Add(employee1);
             Employee employee2 = new Employee();
             employee2.fName = "Joe";
             employee2.lName = "Brown";
             employee2.IDnumber = 2;
+            empList.Add(employee2);
             Employee employee3 = new Employee();
             employee3.fName = "Leo";
             employee3.lName = "NinjaTurtle";
             employee3.IDnumber = 3;
+            empList.Add(employee3);
             Employee employee4 = new Employee();
             employee4.fName = "Don";
             employee4.lName = "NinjaTurtle";
             employee4.IDnumber = 4;
+            empList.Add(employee4);
             Employee employee5 = new Employee();
             employee5.fName = "Raf";
             employee5.lName = "NinjaTurtle";
             employee5.IDnumber = 5;
+            empList.Add(employee5);
             Employee employee6 = new Employee();
             employee6.fName = "Mike";
             employee6.lName = "NinjaTurtle";
             employee6.IDnumber = 6;
+            empList.Add(employee6);
             Employee employee7 = new Employee();
             employee7.fName = "Milo";
             employee7.lName = "Adventurer";
             employee7.IDnumber = 7;
+            empList.Add(employee7);
             Employee employee8 = new Employee();
             employee8.fName = "Otis";
             employee8.lName = "Adventurer";
             employee8.IDnumber = 8;
+            empList.Add(employee8);
             Employee employee9 = new Employee();
             employee9.fName = "Trigger";
             employee9.lName = "Rogers";
             employee9.IDnumber = 9;
+            empList.Add(employee9);
             Employee employee10 = new Employee();
             employee10.fName = "Dug";
             employee10.lName = "Up";
             employee10.IDnumber = 10;
+            empList.Add(employee10);
 
-            List<string> EmpFirstNames = new List<string>();
-            EmpFirstNames.Add(employee1.fName);
-            EmpFirstNames.Add(employee2.fName);
-            EmpFirstNames.Add(employee3.fName);
-            EmpFirstNames.Add(employee4.fName);
-            EmpFirstNames.Add(employee5.fName);
-            EmpFirstNames.Add(employee6.fName);
-            EmpFirstNames.Add(employee7.fName);
-            EmpFirstNames.Add(employee8.fName);
-            EmpFirstNames.Add(employee9.fName);
-            EmpFirstNames.Add(employee10.fName);
 
 
 
             // foreach loop for section 2...
 
-            //foreach (string fname in EmpFirstNames)
+            //foreach (Employee employee in empList)
             //{
-            //    if (fname == "Joe")
+            //    if (employee.fName == "Joe")
             //    {
-            //        JoeList.Add(fname);
+            //        selectedEmployee.Add(employee);                   
             //    }
             //}
-            //foreach (string name in JoeList)
+            //Console.WriteLine("Information for all employees named Joe: ");
+            //foreach (Employee employee in selectedEmployee)
             //{
-            //    Console.WriteLine(name);
+            //    Console.WriteLine("ID: " + employee.IDnumber + ": " + employee.lName + ", " + employee.fName);
             //}
             //Console.ReadLine();
 
@@ -92,22 +92,24 @@ namespace StepAssignment202LambdaFunctions
             // ======================= Sections 3: using the lambda expression ============================
 
 
-            //JoeList = EmpFirstNames.Where(x => x.Contains("Joe")).ToList();
-            //foreach (string name in JoeList)
+            //selectedEmployee = empList.Where(x => x.fName == "Joe").ToList();
+            //Console.WriteLine("Information for all employees named Joe: ");
+            //foreach (Employee employee in selectedEmployee)
             //{
-            //    Console.WriteLine(name);
+            //    Console.WriteLine("ID: " + employee.IDnumber + " " + employee.lName + ", " + employee.fName);
             //}
             //Console.ReadLine();
 
 
             // ======================= Sections 4: using the lambda expression ============================
 
-            JoeList = .Where(x => x > 5).ToList();
-            foreach (string fName, string lName, int IDnumber in Employee)
-            {
-                Console.WriteLine("IDNumber: " + IDnumber + ": " + fName + " " + lName);
-            }
-            Console.ReadLine();
+            //selectedEmployee = empList.Where(x => x.IDnumber > 5).ToList();
+            //Console.WriteLine("All employees that have an ID number greater than 5: ");
+            //foreach (Employee employee in selectedEmployee)
+            //{
+            //    Console.WriteLine("ID: " + employee.IDnumber + " " + employee.fName + " " + employee.lName);
+            //}
+            //Console.ReadLine();
         }
     }
 }
