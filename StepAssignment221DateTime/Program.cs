c52fe03cdec20a93c9c7258913d7aee68a6f6eae
SamanthaEpling <alleycatace@yahoo.com>
2020-03-19 20:37:12 -0700

    StepAssignment 221 DateTime
    Pushing final code for Step/Assignment # 221 (DateTime); C# & .NET Framework Course; The Tech Academy; March 19, 2020.

---
+++ StepAssignment221DateTime/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace StepAssignment221DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            
+            Console.WriteLine("The current time is: " + currentTime.ToString() + "\r\n\nType in an integer and let us calculate what time it will be in that many hours...\r\n");
+            double answer = Convert.ToDouble(Console.ReadLine());
+            DateTime futureTime = currentTime.AddHours(answer);
+            Console.WriteLine("\r\n" + futureTime + " is " + answer + " hours away from " + currentTime + ".\r\n\nThanks for looking into the future with us!");
+            Console.ReadLine();
+        }
+    }
+}
