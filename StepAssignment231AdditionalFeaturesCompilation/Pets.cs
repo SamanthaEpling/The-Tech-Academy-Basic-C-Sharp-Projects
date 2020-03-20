5201a4530927eef7b8d1ee0b483850bff8c4c326
SamanthaEpling <alleycatace@yahoo.com>
2020-03-20 11:41:50 -0700

    StepAssignment231AdditionalFeaturesCompilation
    Pushing final code for Step/Assignment # 231 (Additional Features Compilation); C# & .NET Framework Course; The Tech Academy; March 20, 2020.

---
+++ StepAssignment231AdditionalFeaturesCompilation/Pets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment231AdditionalFeaturesCompilation
+{
+    class Pets
+    {
+        public Pets(string name) : this(name, 5)
+        { }
+        public Pets(string name, int startingAge)
+        {
+            Name = name;
+            Age = startingAge;
+
+        }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public Guid Id { get; set; }
+    }
+}
---
+++ StepAssignment231AdditionalFeaturesCompilation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StepAssignment231AdditionalFeaturesCompilation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===== Section 1: Create a const variable =====
+            const string familyName = "Barking Mad Farms";
+            Console.WriteLine("Welcome to {0}! Where we are mad to have so many pets!\r\n", familyName);
+            //===== Section 2: Create a variable using "var" =====
+            var numberPets = 6;
+            Console.WriteLine("Currently we have {0} pets!\r\n", numberPets);
+            //===== Section 3: Chain two constructors together =====
+            string name = "Lionel";
+            Pets pet = new Pets(name);
+            Console.WriteLine("The latest pet to join our crew, {0} makes the minimum age of all the pets {1} years old.", pet.Name, pet.Age);
+            Console.ReadLine();
+        }
+    }
+}
